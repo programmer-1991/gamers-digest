dcf4c3a83cd519e53d5dccb780ba1d962f05895a
programmer-91 <aminrhimi.1991@gmail.com>
2024-03-05 02:12:19 +0000

    Set secret key in the environment

--- gamers_digest/settings.py
+++ gamers_digest/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^1xamy-s##4b_(ju7-^owtpw8^d*)&k==pu_n=pz()0p(*f9mv'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
