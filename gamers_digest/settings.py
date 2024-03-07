e10027d300f39ac83c0e179062367d920e440191
programmer-91 <aminrhimi.1991@gmail.com>
2024-03-07 01:56:52 +0000

    Register post model andadd trusted origins for requests

--- gamers_digest/settings.py
+++ gamers_digest/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-programmer1-gamersdiges-9qk2amt6z26.ws-eu108.gitpod.io', '.herokuapp.com']
 
@@ -88,6 +88,11 @@ WSGI_APPLICATION = 'gamers_digest.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.gitpod.io",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
