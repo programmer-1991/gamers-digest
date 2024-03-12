fd28eeb7b673e96eccc26ac821b536c75384493e
programmer-91 <aminrhimi.1991@gmail.com>
2024-03-12 00:45:52 +0000

    Add excerpt and updated on fields to post

--- gamers_digest/settings.py
+++ gamers_digest/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-programmer1-gamersdiges-9qk2amt6z26.ws-eu108.gitpod.io', '.herokuapp.com']
 
--- news/models.py
+++ news/models.py
@@ -10,4 +10,6 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete = models.CASCADE, related_name = "news_posts")
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add = True)
+    updated_on = models.DateTimeField(auto_now = True)
     status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank = True)
