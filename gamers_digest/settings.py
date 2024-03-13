78216eb12fb76d70405b1f72418e12cacbaced86
programmer-91 <aminrhimi.1991@gmail.com>
2024-03-13 01:49:58 +0000

    Modify local host

--- gamers_digest/settings.py
+++ gamers_digest/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-programmer1-gamersdiges-9qk2amt6z26.ws-eu110.gitpod.io', '.herokuapp.com']
+ALLOWED_HOSTS = ['.gitpod.io', '.herokuapp.com']
 
 # Application definition
 
