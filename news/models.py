858194a97f6bf6ebc285f8dfde078ae4623625f7
programmer-91 <aminrhimi.1991@gmail.com>
2024-03-13 02:35:34 +0000

    Remove irrelevant fields from pthe ost model

--- news/models.py
+++ news/models.py
@@ -1,18 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-STATUS = ((0, "draft"), (1, "published"))
-
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=256, unique=True)
     slug = models.SlugField(max_length=256, unique=True)
-    author = models.ForeignKey(User, on_delete = models.CASCADE, related_name = "news_posts")
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add = True)
     updated_on = models.DateTimeField(auto_now = True)
-    status = models.IntegerField(choices=STATUS, default=0)
-    excerpt = models.TextField(blank = True)
     class Meta:
         ordering = ["-created_on"]
     def __str__(self):
