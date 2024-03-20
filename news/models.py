e2eb34b7225ffed84bd6fcf9b2a9455337ad299d
programmer-91 <aminrhimi.1991@gmail.com>
2024-03-20 07:42:58 +0000

    Add game model and relate it to the post model

--- news/models.py
+++ news/models.py
@@ -2,10 +2,21 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Game(models.Model):
+    title = models.CharField(max_length=32, unique=True)
+    genre = models.CharField(max_length=32)
+    developer = models.CharField(max_length=32)
+    publisher = models.CharField(max_length=32)
+    release = models.DateField()
+    def __str__(self):
+        return f"{self.title}"
+
 class Post(models.Model):
     title = models.CharField(max_length=256, unique=True)
     slug = models.SlugField(max_length=256, unique=True)
     content = models.TextField()
+    topic = models.ForeignKey(Game, on_delete = models.CASCADE, related_name = "news_posts", default=1)
     created_on = models.DateTimeField(auto_now_add = True)
     updated_on = models.DateTimeField(auto_now = True)
     class Meta:
