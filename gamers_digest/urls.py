b7616981baee220f8e1b36d6bf56393f3680b174
programmer-91 <aminrhimi.1991@gmail.com>
2024-03-26 01:33:44 +0000

    Add path for summernote

--- gamers_digest/urls.py
+++ gamers_digest/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('', include('news.urls'), name='news.urls'),
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
+    path('', include('news.urls'), name='news.urls'),
+    
 ]
