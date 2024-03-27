ae7366b4485c5c5a7f21ff267769995dde66b397
programmer-91 <aminrhimi.1991@gmail.com>
2024-03-27 00:51:41 +0000

    Update the news app to use summernote

--- news/admin.py
+++ news/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from .models import Post, Game
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    
+    list_display = ('title', 'slug',)
+    search_fields = ['title']
+    #list_filter = ('',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+
+
 
 # Register your models here.
-admin.site.register(Post)
 admin.site.register(Game)
