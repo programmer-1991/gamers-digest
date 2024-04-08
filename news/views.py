67b7bac853f81e0982ef7843d17245f485b4b92f
programmer-91 <aminrhimi.1991@gmail.com>
2024-04-08 23:46:44 +0000

    Create templates and add styling to it

--- news/views.py
+++ news/views.py
@@ -6,5 +6,6 @@ from .models import Post
 
 class PostList(generic.ListView):
     queryset = Post.objects.all()
-    #template_name = "post_list.html"
+    template_name = "news/index.html"
+    paginate_by = 6
 
