54384bb1872b9dda860aae83c426028c46a9b305
programmer-91 <aminrhimi.1991@gmail.com>
2024-04-12 05:11:23 +0000

    Enable serving of static files

--- gamers_digest/settings.py
+++ gamers_digest/settings.py
@@ -20,6 +20,7 @@ if os.path.isfile('env.py'):
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 
 
 # Quick-start development settings - unsuitable for production
@@ -29,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.gitpod.io', '.herokuapp.com']
 
