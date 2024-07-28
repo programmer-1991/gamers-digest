295bbbc25e997d70da7fc4111ae098a104f99242
Amin Rhimi <aminrhimi.1991@gmail.com>
2024-07-28 13:10:54 +0000

    Put game model first in order

--- news/models.py
+++ news/models.py
@@ -1,21 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+
 # Create your models here.
-
-class Post(models.Model):
-    title = models.CharField(max_length=256, unique=True)
-    slug = models.SlugField(max_length=256, unique=True)
-    intro = models.CharField(max_length=1024, blank=True)
-    content = models.TextField()
-    topic = models.ForeignKey(Game, on_delete = models.CASCADE, related_name = "posts", default=0)
-    created_on = models.DateTimeField(auto_now_add = True)
-    updated_on = models.DateTimeField(auto_now = True)
-    class Meta:
-        ordering = ["-created_on"]
-    def __str__(self):
-        return f"{self.title}"
-
 class Game(models.Model):
     PLATFORM = ((0, ""), (1, "Xbox"), (2, "Playstation"), (3, "Nintendo"), (4, "Windows"))
     title = models.CharField(max_length=256, unique=True)
@@ -30,5 +17,19 @@ class Game(models.Model):
     featured_image = CloudinaryField('image', default='placeholder')
     def __str__(self):
         return f"{self.title}"
+
+class Post(models.Model):
+    title = models.CharField(max_length=256, unique=True)
+    slug = models.SlugField(max_length=256, unique=True)
+    intro = models.CharField(max_length=1024, blank=True)
+    content = models.TextField()
+    topic = models.ForeignKey(Game, on_delete = models.CASCADE, related_name = "posts", default=0)
+    created_on = models.DateTimeField(auto_now_add = True)
+    updated_on = models.DateTimeField(auto_now = True)
+    class Meta:
+        ordering = ["-created_on"]
+    def __str__(self):
+        return f"{self.title}"
+
 
 
